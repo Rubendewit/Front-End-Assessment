24c0eb1ecdd0d448eb6b92e677e1ae5e7448debc
Ruben de Wit <ruben.de.wit@rtl.nl>
2018-06-15 13:50:16 +0200

    Add AppBar, showing page title and possible navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Router, Route } from 'react-router-dom'
+import createHistory from 'history/createBrowserHistory';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import Home from './Home';
-import Article from './Article';
+import Home from './components/Home';
+import Article from './components/Article';
+import AppBar from './components/AppBar';
+import LoadMore from './components/LoadMore';
+
+const browserHistory = createHistory();
 
 const theme = createMuiTheme({
   palette: {
@@ -22,10 +27,10 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-        <Router>
+        <Router history={browserHistory}>
           <div className="App">
             <CssBaseline />
-            <h1>Fashion News</h1>
+            <AppBar />
             <Route exact path="/" component={Home}/>
             <Route path="/article/:id/:slug" component={Article}/>
           </div>
