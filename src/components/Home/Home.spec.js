6e549c8ec69ac7dd2c00f527a2651e90657e5c98
Ruben de Wit <ruben.de.wit@rtl.nl>
2018-06-19 18:38:31 +0200

    Write test for Home.js

---
+++ src/components/Home/Home.spec.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { configure } from 'enzyme';
+import { createShallow } from '@material-ui/core/test-utils';
+import Adapter from 'enzyme-adapter-react-16';
+import sinon from 'sinon';
+import Home from './Home';
+import * as News from '../../getNews.js';
+import { NEWS_STUB, LAST_NEWS_STUB } from '../../../test/stub/newsStub';
+
+configure({ adapter: new Adapter() });
+
+let shallow;
+let sinonStub;
+
+const getArticles = (args) => args.offset === 0 ? NEWS_STUB : LAST_NEWS_STUB;
+
+beforeAll(() => {
+  shallow = createShallow({ dive: true });
+  sinonStub = sinon.stub(News, 'getNewsArticles').returns(NEWS_STUB);
+});
+
+afterAll(() => {
+  sinonStub.restore();
+});
+
+it('should update state with articles', async () => {
+  const wrapper = shallow(<Home />);
+  await wrapper.update();
+
+  const expectedArticles = wrapper.state('newsArticles');
+  const hasNextPage = wrapper.state('hasNextPage');
+
+  expect(hasNextPage).toBe(true);
+  expect(expectedArticles.length).toBe(10);
+  expect(Object.keys(expectedArticles[0])).toEqual(["title", "slug", "imageUrl", "id"]);
+});
---
+++ test/stub/newsStub.js
@@ -0,0 +1,75 @@
+export const NEWS_STUB = {
+  fashionunitedNlNewsArticles: [
+    {
+      title: 'Stub title',
+      slug: 'stub-title',
+      imageUrl: 'https://fashionunited.info/global-assets/img/default/fu-default_1200x630_black-favicon.jpg',
+      id: 1
+    },
+    {
+      title: 'Stub title',
+      slug: 'stub-title',
+      imageUrl: 'https://fashionunited.info/global-assets/img/default/fu-default_1200x630_black-favicon.jpg',
+      id: 2
+    },
+    {
+      title: 'Stub title',
+      slug: 'stub-title',
+      imageUrl: 'https://fashionunited.info/global-assets/img/default/fu-default_1200x630_black-favicon.jpg',
+      id: 3
+    },
+    {
+      title: 'Stub title',
+      slug: 'stub-title',
+      imageUrl: 'https://fashionunited.info/global-assets/img/default/fu-default_1200x630_black-favicon.jpg',
+      id: 4
+    },
+    {
+      title: 'Stub title',
+      slug: 'stub-title',
+      imageUrl: 'https://fashionunited.info/global-assets/img/default/fu-default_1200x630_black-favicon.jpg',
+      id: 5
+    },
+    {
+      title: 'Stub title',
+      slug: 'stub-title',
+      imageUrl: 'https://fashionunited.info/global-assets/img/default/fu-default_1200x630_black-favicon.jpg',
+      id: 6
+    },
+    {
+      title: 'Stub title',
+      slug: 'stub-title',
+      imageUrl: 'https://fashionunited.info/global-assets/img/default/fu-default_1200x630_black-favicon.jpg',
+      id: 7
+    },
+    {
+      title: 'Stub title',
+      slug: 'stub-title',
+      imageUrl: 'https://fashionunited.info/global-assets/img/default/fu-default_1200x630_black-favicon.jpg',
+      id: 8
+    },
+    {
+      title: 'Stub title',
+      slug: 'stub-title',
+      imageUrl: 'https://fashionunited.info/global-assets/img/default/fu-default_1200x630_black-favicon.jpg',
+      id: 9
+    },
+    {
+      title: 'Stub title',
+      slug: 'stub-title',
+      imageUrl: 'https://fashionunited.info/global-assets/img/default/fu-default_1200x630_black-favicon.jpg',
+      id: 10
+    },
+  ]
+};
+
+export const LAST_NEWS_STUB = {
+  fashionunitedNlNewsArticles: [
+    {
+      title: 'Last stub title',
+      slug: 'last-stub-title',
+      imageUrl: 'https://fashionunited.info/global-assets/img/default/fu-default_1200x630_black-favicon.jpg',
+      id: 20
+    }
+  ]
+}
