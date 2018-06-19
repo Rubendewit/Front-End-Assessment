74915f2d708f8e73b6844b96cb75b0df3aa264f6
Ruben de Wit <ruben.de.wit@rtl.nl>
2018-06-19 08:20:57 +0200

    Cleanup LoadMore component Turn LoadMore component into a function

--- src/components/LoadMore.jsx
+++ src/components/LoadMore.jsx
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import { Button } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import ArrowDownward from '@material-ui/icons/ArrowDownward';
 
 const styles = {
-  root: {
-    flexGrow: 1,
-  },
   wrapper: {
     display: 'flex',
     flexDirection: 'column',
@@ -16,32 +12,13 @@ const styles = {
   }
 };
 
-class LoadMore extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      expanded: false
-    };
-  }
-  
-  render() {
-    const { classes, onClick, offset, isVisible } = this.props;
-
-    return isVisible ? (
-      <div className={classes.root}>
-        <div className={classes.wrapper} onClick={() => onClick(offset)}>
-          <Button size="small" color="primary">Load more</Button>
-          <ArrowDownward />
-        </div>          
-      </div>
-    ) : null;
-  };
-}
-
-LoadMore.propTypes = {
-  classes: PropTypes.object.isRequired,
-  onClick: PropTypes.func
+const LoadMore = ({ classes, onClick, offset, isVisible }) => {
+  return isVisible ? (
+    <div className={classes.wrapper} onClick={() => onClick(offset)}>
+      <Button size="small" color="primary">Load more</Button>
+      <ArrowDownward />
+    </div>
+  ) : null;
 };
 
 export default withStyles(styles)(LoadMore);
